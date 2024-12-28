c9fdf8ac95686a48d4f9052811512b9433d1ebdc
Loren <lorengrx@gmail.com>
2024-12-28 02:23:19 +0000

    search feature

--- posts-app/src/app/pages/posts-list/posts-list.ts
+++ posts-app/src/app/pages/posts-list/posts-list.ts
@@ -1,28 +1,39 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store, StoreModule } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { combineLatest, map, Observable } from 'rxjs';
 import { Post } from '../../core/state/posts.state';
-import { selectPosts, selectTotalPosts } from '../../core/state/posts.selectors';
+import { selectPosts, selectSearch, selectTotalPosts } from '../../core/state/posts.selectors';
 import { TruncatePipe } from '../../shared/pipes/truncate.pipe';
 import { CommonModule } from '@angular/common';
-import { loadPosts } from '../../core/state/posts.actions';
+import { loadPosts, updateSearch } from '../../core/state/posts.actions';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { FormControl, FormsModule } from '@angular/forms';
+import { MatFormField, MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector: 'app-posts-list',
     templateUrl: './posts-list.html',
     standalone: true,
-    imports: [TruncatePipe, CommonModule, StoreModule, MatPaginatorModule]
+    imports: [TruncatePipe, CommonModule, StoreModule, FormsModule, MatFormFieldModule, MatFormField, MatInputModule, MatPaginatorModule]
 })
 export class PostsListComponent {
     posts$: Observable<Post[]>;
     totalPosts$: Observable<number>;
+    filteredPosts$: Observable<Post[]> | undefined;
     currentPage: number = 1;
     limit: number = 10;
+    searchTerm$: Observable<string>;
+    searchTerm: string = "";
 
     ngOnInit(): void {
         this.store.dispatch(loadPosts({ page: this.currentPage, limit: this.limit }));
+        this.filteredPosts$ = combineLatest([this.posts$, this.searchTerm$]).pipe(
+            map(([posts, searchTerm]) =>
+                posts.filter(post => post.title.toLowerCase().includes(searchTerm.toLowerCase()))
+            )
+        );
     }
 
     onPageChange(event: PageEvent) {
@@ -30,9 +41,16 @@ export class PostsListComponent {
         this.store.dispatch(loadPosts({ page: this.currentPage, limit: event.pageSize }));
     }
 
+    onSearchChange(searchString: string) {
+        this.store.dispatch(updateSearch({ search: searchString }));
+    }
+
+
+
     constructor(private store: Store, private router: Router) {
         this.posts$ = this.store.select(selectPosts);
         this.totalPosts$ = this.store.select(selectTotalPosts);
+        this.searchTerm$ = this.store.select(selectSearch);
     }
 
     navigateToPost(id: string) {
